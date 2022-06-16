224d6b4522b11429ff00d3f57a50f571b9d7ce4f
dibenedetto <dibenedettojonatan@hotmail.com>
2022-06-16 19:52:28 +0200

    Primer etapa finalizada

---
+++ src/date/Monedas.jsx
@@ -0,0 +1,13 @@
+
+
+
+ const monedas = [
+    {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'ARG', nombre: 'Peso Argentina'},
+    {id: 'GBP', nombre: 'Libra Esterlina'} 
+]
+
+export{
+    monedas
+}
